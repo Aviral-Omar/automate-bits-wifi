9a82ef8a1c03d61232beb8cfc764f3dc3307c48e
Aviral-Omar <aviralomar0301@gmail.com>
2023-01-20 00:36:39 +0530

    deleted js files

--- autologin-win.ps1
+++ autologin-win.ps1
@@ -1,7 +1,7 @@
 $Network = (get-netconnectionProfile).Name
 if ($Network -match "BITS-(STUDENT|STAFF)") {
-	$Username = "F20190411"
-	$Password = "B3PS049854#"
+	$Username = ""
+	$Password = ""
 	$Time = ((Get-Date -UFormat %s) -Replace("[,\.]", "")).Substring(0,13)
 	$Url = "https://fw.bits-pilani.ac.in:8090/httpclient.html"
 	$Params = @{mode=191;username=$Username;password=$Password;
