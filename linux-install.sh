dd8fda0d4f4e50be8c28654d6c48a32f4a412a8c
Aviral-Omar <aviralomar0301@gmail.com>
2023-01-20 02:27:43 +0530

    changed linux install script

--- linux-install.sh
+++ linux-install.sh
@@ -4,7 +4,8 @@ if [ -d "/etc/NetworkManager/dispatcher.d" ]; then
 	sudo chown root:root /etc/NetworkManager/dispatcher.d/autologin-linux
 	sudo chmod +x /etc/NetworkManager/dispatcher.d/autologin-linux
 	sudo systemctl restart NetworkManager
-elif [ -d "/etc/network/if-up.d" ]; then
+fi
+if [ -d "/etc/network/if-up.d" ]; then
 	sudo cp autologin-linux /etc/network/if-up.d
 	sudo chown root:root /etc/network/if-up.d/autologin-linux
 	sudo chmod +x /etc/network/if-up.d/autologin-linux
