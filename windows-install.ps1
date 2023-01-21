a3241f1278d2f06852445db873b9187d38a88dfd
Aviral-Omar <aviralomar0301@gmail.com>
2023-01-21 15:13:46 +0530

    modified windows script settings

--- windows-install.ps1
+++ windows-install.ps1
@@ -1,8 +1,7 @@
-# schtasks /create /tn "Net-Connected" /tr "powershell -executionpolicy bypass -file '%SYSTEMDRIVE%\autologin.ps1'" /SC ONEVENT /EC Microsoft-Windows-NetworkProfile/Operational /MO "*[System[Provider[@Name='Microsoft-Windows-NetworkProfile'] and EventID=10000]]" /RU "System" /F
 Copy-Item -Path ".\autologin-win.ps1" -Destination "C:\"
 
 $Name = "Automate WiFi Login"
-$Action = New-ScheduledTaskAction  -Execute 'Powershell.exe' -Argument "-ExecutionPolicy Unrestricted -File C:\autologin-win.ps1"
+$Action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-ExecutionPolicy Unrestricted -File C:\autologin-win.ps1"
 
 $CIMTriggerClass = Get-CimClass -ClassName MSFT_TaskEventTrigger -Namespace Root/Microsoft/Windows/TaskScheduler:MSFT_TaskEventTrigger
 $Trigger = New-CimInstance -CimClass $CIMTriggerClass -ClientOnly
@@ -12,4 +11,7 @@ $Trigger.Subscription =
 "@
 $Trigger.Enabled = $True
 
-Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName $Name -Description 'Automating logging in to BITS WiFi.' -User 'System' -Force
+$ExecutionTime = New-TimeSpan -Minutes 1
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -ExecutionTimeLimit $ExecutionTime -MultipleInstances Parallel -StartWhenAvailable
+
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName $Name -Description 'Automating logging in to BITS WiFi.' -Settings $Settings -User 'System' -Force
